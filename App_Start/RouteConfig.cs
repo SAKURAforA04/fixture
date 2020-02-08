5464011629e9a75e92b32ef29d26f1bf4a2ab4eb
1349621303 <1349621303@qq.com>
2020-02-08 21:12:57 +0800

    最新稳定版本

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,6 +13,7 @@ namespace Fixture02
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
