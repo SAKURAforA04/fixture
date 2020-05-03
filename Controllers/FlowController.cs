149cbf7073c3959a937d4cc88849e57ea63d9de3
xzmt <xzmt_kz@hotmail.com>
2020-05-03 11:58:28 +0800

    其他部分流程

--- Controllers/FlowController.cs
+++ Controllers/FlowController.cs
@@ -15,13 +15,16 @@ namespace Fixture02.Controllers
         public ActionResult Index(String code, String name, String location, String model, String partNo)
         {
             var flow = (from c in db.Inout where c.Code.Contains(code) select new flower { Code = c.Code,State = c.State ,Date = c.AddDate })
-                .Union(from e in db.Jigitem where e.Code.Contains(code) select new flower { Code = e.Code, State = e.State, Date = e.AddDate })
+                .Union(from e in db.Jigitem where e.Code.Contains(code) select new flower { Code = e.Code, State = "库存", Date = e.AddDate })
+                .Union(from e in db.repair where e.Code.Contains(code) select new flower { Code = e.Code, State = "报修", Date = e.AddDate })
+                .Union(from e in db.repair where e.Code.Contains(code) select new flower { Code = e.Code, State = "库存", Date = e.RepairDate })
+                .Union(from e in db.ScrapSet where e.Code.Contains(code) && e.ScrapState.Contains("报废")select new flower { Code = e.Code, State = "报废", Date = e.SecondReviewDate })
+                .Union(from e in db.Check where e.Code.Contains(code) select new flower { Code = e.Code, State = "点检", Date = e.AddDate })
                .OrderBy(t => t.Date)
                ;
 
-
             FlowerList flowerList = new FlowerList();
-            List<System.DateTime> Time = new List<System.DateTime>();
+            List<Nullable<System.DateTime>> Time = new List<Nullable<System.DateTime>>();
             List<int> State = new List<int>();
             flowerList.flowerModelList = new List<flower>();
             foreach (var flowermodel in flow)
@@ -68,7 +71,7 @@ namespace Fixture02.Controllers
     {
         public string Code { get; set; }
         public string State { get; set; }
-        public System.DateTime Date { get; set; }
+        public Nullable<System.DateTime> Date { get; set; }
     }
     public class FlowerList
     {
